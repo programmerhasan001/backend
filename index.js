4d09be55ac11f0f93b9c18db27fe32baaeb0eeb4
programmerhasan001 <programmermdhasan@gmail.com>
2025-08-03 17:35:02 +0600

    custom middleware

--- index.js
+++ index.js
@@ -1,35 +1,23 @@
 import express from "express"
-const app = express()
-const port = 3002
+import morgan from "morgan"
 
-app.use(express.json());
-app.use(express.urlencoded({extended: true}))
+const app = express()
+const port = 3002;
+
+const logger = (req, res, next) => {
+    console.log("Method =", req.method, " URL =", req.url);
+    next();
+}
+
+app.use(morgan('short'))
+app.use(logger)
 
 app.get('/', (req, res) => {
-  res.send(`
-    <form method="POST" action="/user">
-      Name: <input type="text" name="user[name]" /><br />
-      Age: <input type="text" name="user[age]" /><br />
-      Skills: 
-      <input type="text" name="user[skills][]" placeholder="Skill 1" />
-      <input type="text" name="user[skills][]" placeholder="Skill 2" /><br />
-      <button type="submit">Submit</button>
-    </form>
-  `);
-});
-
-app.post("/blog", (req, res) => {
-    const data = req.body
-
-    res.send(data);
-})
-
-app.post("/user", (req, res) => {
-    const data = req.body
-    console.log(data)
-    res.status(201).json(data)
+    // console.log("get morgan ot")
+    res.status(200).json({message: "hello"})
 })
 
 app.listen(port, () => {
-    console.log(`server running on port ${port}`)
+    console.log(`server started at port ${port}`)
 })
+
