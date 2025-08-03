df668570d0f6a9a716748f66ea5935c940dce89e
programmerhasan001 <programmermdhasan@gmail.com>
2025-08-03 15:28:47 +0600

    http requests

--- index.js
+++ index.js
@@ -6,6 +6,26 @@ app.get("/", (req, res) => {
     res.status(200).json({message: "success"});
 })
 
+app.get("/user", (req, res) => {
+    res.sendStatus(200);
+})
+
+app.post("/user", (req, res) => {
+    res.sendStatus(201);
+})
+
+app.put("/user", (req, res) => {
+    res.sendStatus(200);
+})
+
+app.patch("/user", (req, res) => {
+    res.sendStatus(200);
+})
+
+app.delete("/user", (req, res) => {
+    res.sendStatus(200);
+})
+
 app.listen(port, () => {
     console.log(`server running on port ${port}`)
 })
