1b8c432fdc339855adcee83f745b0b1f9f1184ac
programmerhasan001 <programmermdhasan@gmail.com>
2025-08-04 16:22:45 +0600

    ejs-tags

--- index.js
+++ index.js
@@ -7,18 +7,10 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
 app.get("/", (req, res) => {
-    let today = new Date()
-    let day = today.getDay()
-
-    let dayType = "a weekday";
-    let advice = "it's time to work hard";
-
-    if(day === 0 || day === 6) {
-        dayType = "the weekend";
-        advice = "it's time to have fun!";
-    }
-    
-    res.render(`index.ejs`, {dayType, advice})
+    let bowl = ["Apples", "Oranges", "Pears"]
+    let htmlContent = "<h2>HTML Content</h2>"
+    let numbers = [1, 2, 3, 4];
+    res.render(`index.ejs`, {fruits: bowl, htmlContent, numbers})
 })
 
 app.listen(port, () => {
