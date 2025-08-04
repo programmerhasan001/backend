0e5305719dd88008577b1f4790a9a9247fffad2d
programmerhasan001 <programmermdhasan@gmail.com>
2025-08-04 18:47:35 +0600

    static file serve

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = 3002
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
 
 app.get("/", (req, res) => {
     res.render(`index.ejs`)
