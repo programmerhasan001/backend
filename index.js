6e5ac2a82c76aab537bd3d52d0f7e88abe7a41d3
programmerhasan001 <programmermdhasan@gmail.com>
2025-08-07 15:41:03 +0600

    styling

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = 3002;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
 // An array of random adjectives in English
 const randomAdjectives = ["Happy", "Sad", "Funny", "Crazy"];
